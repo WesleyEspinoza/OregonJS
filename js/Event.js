f777593714a285f725fe12783371c986fc04648c
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-07-30 16:24:46 -0700

    finish refactor

--- js/Event.js
+++ js/Event.js
@@ -1,186 +1,223 @@
-var OregonH = OregonH || {};
- 
-OregonH.Event = {};
- 
-OregonH.Event.eventTypes = [
-  {
-    type: 'STAT-CHANGE',
-    notification: 'negative',
-    stat: 'crew',
-    value: -3,
-    text: 'Food intoxication. Casualties: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'negative',
-    stat: 'crew',
-    value: -4,
-    text: 'Flu outbreak. Casualties: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'negative',
-    stat: 'food',
-    value: -10,
-    text: 'Worm infestation. Food lost: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'negative',
-    stat: 'money',
-    value: -50,
-    text: 'Pick pockets steal $'
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'negative',
-    stat: 'oxen',
-    value: -1,
-    text: 'Ox flu outbreak. Casualties: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'positive',
-    stat: 'food',
-    value: 20,
-    text: 'Found wild berries. Food added: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'positive',
-    stat: 'food',
-    value: 20,
-    text: 'Found wild berries. Food added: '
-  },
-  {
-    type: 'STAT-CHANGE',
-    notification: 'positive',
-    stat: 'oxen',
-    value: 1,
-    text: 'Found wild oxen. New oxen: '
-  },
-  {
-    type: 'SHOP',
-    notification: 'neutral',
-    text: 'You have found a shop',
-    products: [
-      {item: 'food', qty: 20, price: 50},
-      {item: 'oxen', qty: 1, price: 200},
-      {item: 'firepower', qty: 2, price: 50},
-      {item: 'crew', qty: 5, price: 80}
-    ]
-  },
-  {
-    type: 'SHOP',
-    notification: 'neutral',
-    text: 'You have found a shop',
-    products: [
-      {item: 'food', qty: 30, price: 50},
-      {item: 'oxen', qty: 1, price: 200},
-      {item: 'firepower', qty: 2, price: 20},
-      {item: 'crew', qty: 10, price: 80}
-    ]
-  },
-  {
-    type: 'SHOP',
-    notification: 'neutral',
-    text: 'Smugglers sell various goods',
-    products: [
-      {item: 'food', qty: 20, price: 60},
-      {item: 'oxen', qty: 1, price: 300},
-      {item: 'firepower', qty: 2, price: 80},
-      {item: 'crew', qty: 5, price: 60}
-    ]
-  },
-  {
-    type: 'ATTACK',
-    notification: 'negative',
-    text: 'Bandits are attacking you'
-  },
-  {
-    type: 'ATTACK',
-    notification: 'negative',
-    text: 'Bandits are attacking you'
-  },
-  {
-    type: 'ATTACK',
-    notification: 'negative',
-    text: 'Bandits are attacking you'
+/* eslint-disable brace-style */
+/* eslint-disable func-names */
+
+
+// eslint-disable-next-line no-var
+// eslint-disable-next-line no-var
+
+
+class GameEvent {
+  constructor(game) {
+    this.game = game;
+    this.eventsData = [
+      {
+        type: 'STAT-CHANGE',
+        notification: 'negative',
+        stat: 'crew',
+        value: -3,
+        text: 'Food intoxication. Casualties: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'negative',
+        stat: 'crew',
+        value: -4,
+        text: 'Flu outbreak. Casualties: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'negative',
+        stat: 'food',
+        value: -10,
+        text: 'Worm infestation. Food lost: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'negative',
+        stat: 'money',
+        value: -50,
+        text: 'Pick pockets steal $',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'negative',
+        stat: 'oxen',
+        value: -1,
+        text: 'Ox flu outbreak. Casualties: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'positive',
+        stat: 'food',
+        value: 20,
+        text: 'Found wild berries. Food added: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'positive',
+        stat: 'food',
+        value: 20,
+        text: 'Found wild berries. Food added: ',
+      },
+      {
+        type: 'STAT-CHANGE',
+        notification: 'positive',
+        stat: 'oxen',
+        value: 1,
+        text: 'Found wild oxen. New oxen: ',
+      },
+      {
+        type: 'SHOP',
+        notification: 'neutral',
+        text: 'You have found a shop',
+        products: [
+          { item: 'food', qty: 20, price: 50 },
+          { item: 'oxen', qty: 1, price: 200 },
+          { item: 'firepower', qty: 2, price: 50 },
+          { item: 'crew', qty: 5, price: 80 },
+        ],
+      },
+      {
+        type: 'SHOP',
+        notification: 'neutral',
+        text: 'You have found a shop',
+        products: [
+          { item: 'food', qty: 30, price: 50 },
+          { item: 'oxen', qty: 1, price: 200 },
+          { item: 'firepower', qty: 2, price: 20 },
+          { item: 'crew', qty: 10, price: 80 },
+        ],
+      },
+      {
+        type: 'SHOP',
+        notification: 'neutral',
+        text: 'Smugglers sell various goods',
+        products: [
+          { item: 'food', qty: 20, price: 60 },
+          { item: 'oxen', qty: 1, price: 300 },
+          { item: 'firepower', qty: 2, price: 80 },
+          { item: 'crew', qty: 5, price: 60 },
+        ],
+      },
+      {
+        type: 'ATTACK',
+        notification: 'negative',
+        text: 'Bandits are attacking you',
+      },
+      {
+        type: 'ATTACK',
+        notification: 'negative',
+        text: 'Bandits are attacking you',
+      },
+      {
+        type: 'ATTACK',
+        notification: 'negative',
+        text: 'Bandits are attacking you',
+      },
+    ];
+    this.eventTypes = [];
+    this.populateEvents(this.eventsData);
   }
-];
- 
-OregonH.Event.generateEvent = function(){
-  //pick random one
-  var eventIndex = Math.floor(Math.random() * this.eventTypes.length);
-  var eventData = this.eventTypes[eventIndex];
- 
-  //events that consist in updating a stat
-  if(eventData.type == 'STAT-CHANGE') {
-    this.stateChangeEvent(eventData);
+
+
+  populateEvents(data) {
+    this.eventTypes = data;
   }
- 
-  //shops
-  else if(eventData.type == 'SHOP') {
-    //pause game
-    this.game.pauseJourney();
- 
-    //notify user
-    this.ui.notify(eventData.text, eventData.notification);
- 
-    //prepare event
-    this.shopEvent(eventData);
+
+  generateEvent() {
+    // pick random one
+    const eventIndex = Math.floor(Math.random() * this.eventTypes.length);
+    const eventData = this.eventTypes[eventIndex];
+
+    // events that consist in updating a stat
+    if (eventData.type === 'STAT-CHANGE') {
+      this.stateChangeEvent(eventData);
+    }
+
+    // shops
+    else if (eventData.type === 'SHOP') {
+    // pause game
+      this.game.pauseJourney();
+
+      // notify user
+      this.ui.notify(eventData.text, eventData.notification);
+
+      // prepare event
+      this.shopEvent(eventData);
+    }
+
+    // attacks
+    else if (eventData.type === 'ATTACK') {
+    // pause game
+      this.game.pauseJourney();
+
+      // notify user
+      this.ui.notify(eventData.text, eventData.notification);
+
+      // prepare event
+      this.attackEvent(eventData);
+    }
   }
- 
-  //attacks
-  else if(eventData.type == 'ATTACK') {
-    //pause game
-    this.game.pauseJourney();
- 
-    //notify user
-    this.ui.notify(eventData.text, eventData.notification);
- 
-    //prepare event
-    this.attackEvent(eventData);
+
+
+  attackEvent() {
+    const firepower = Math.round((0.7 + 0.6 * Math.random()) * this.game.ENEMY_FIREPOWER_AVG);
+    const gold = Math.round((0.7 + 0.6 * Math.random()) * this.game.ENEMY_GOLD_AVG);
+
+    this.ui.showAttack(firepower, gold);
   }
-};
- 
-OregonH.Event.stateChangeEvent = function(eventData) {
-  //can't have negative quantities
-  if(eventData.value + this.caravan[eventData.stat] >= 0) {
-    this.caravan[eventData.stat] += eventData.value;
-    this.ui.notify(eventData.text + Math.abs(eventData.value), eventData.notification);
+
+
+  stateChangeEvent(eventData) {
+    // can't have negative quantities
+    if (eventData.value + this.caravan[eventData.stat] >= 0) {
+      this.caravan[eventData.stat] += eventData.value;
+      this.ui.notify(eventData.text + Math.abs(eventData.value), eventData.notification);
+    }
   }
-};
- 
-OregonH.Event.shopEvent = function(eventData) {
-  //number of products for sale
-  var numProds = Math.ceil(Math.random() * 4);
- 
-  //product list
-  var products = [];
-  var j, priceFactor;
- 
-  for(var i = 0; i < numProds; i++) {
-    //random product
-    j = Math.floor(Math.random() * eventData.products.length);
- 
-    //multiply price by random factor +-30%
-    priceFactor = 0.7 + 0.6 * Math.random();
- 
-    products.push({
-      item: eventData.products[j].item,
-      qty: eventData.products[j].qty,
-      price: Math.round(eventData.products[j].price * priceFactor)
-    });
+
+
+  shopEvent(eventData) {
+  // number of products for sale
+    const numProds = Math.ceil(Math.random() * 4);
+
+    // product list
+    const products = [];
+    let j; let
+      priceFactor;
+
+    for (let i = 0; i < numProds; i += 1) {
+    // random product
+      j = Math.floor(Math.random() * eventData.products.length);
+
+      // multiply price by random factor +-30%
+      priceFactor = 0.7 + 0.6 * Math.random();
+
+      products.push({
+        item: eventData.products[j].item,
+        qty: eventData.products[j].qty,
+        price: Math.round(eventData.products[j].price * priceFactor),
+      });
+    }
+
+    this.ui.showShop(products);
   }
- 
-  this.ui.showShop(products);
-};
- 
-//prepare an attack event
-OregonH.Event.attackEvent = function(eventData){
-  var firepower = Math.round((0.7 + 0.6 * Math.random()) * OregonH.ENEMY_FIREPOWER_AVG);
-  var gold = Math.round((0.7 + 0.6 * Math.random()) * OregonH.ENEMY_GOLD_AVG);
- 
-  this.ui.showAttack(firepower, gold);
-};
+}
+
+
+// OregonH.Event.generateEvent = function () {
+
+// };
+
+// OregonH.Event.stateChangeEvent = function (eventData) {
+
+// };
+
+// OregonH.Event.shopEvent = function (eventData) {
+
+// };
+
+// prepare an attack event
+// OregonH.Event.attackEvent = function (eventData) {
+
+// };
--- js/UI.js
+++ js/UI.js
@@ -1,168 +1,199 @@
-var OregonH = OregonH || {};
- 
-OregonH.UI = {};
- 
-//show a notification in the message area
-OregonH.UI.notify = function(message, type){
-  document.getElementById('updates-area').innerHTML = '<div class="update-' + type + '">Day '+ Math.ceil(this.caravan.day) + ': ' + message+'</div>' + document.getElementById('updates-area').innerHTML;
-};
- 
-//refresh visual caravan stats
-OregonH.UI.refreshStats = function() {
-  //modify the dom
-  document.getElementById('stat-day').innerHTML = Math.ceil(this.caravan.day);
-  document.getElementById('stat-distance').innerHTML = Math.floor(this.caravan.distance);
-  document.getElementById('stat-crew').innerHTML = this.caravan.crew;
-  document.getElementById('stat-oxen').innerHTML = this.caravan.oxen;
-  document.getElementById('stat-food').innerHTML = Math.ceil(this.caravan.food);
-  document.getElementById('stat-money').innerHTML = this.caravan.money;
-  document.getElementById('stat-firepower').innerHTML = this.caravan.firepower;
-  document.getElementById('stat-weight').innerHTML = Math.ceil(this.caravan.weight) + '/' + this.caravan.capacity;
- 
-  //update caravan position
-  document.getElementById('caravan').style.left = (380 * this.caravan.distance/OregonH.FINAL_DISTANCE) + 'px';
-};
+/* eslint-disable func-names */
 
-//show attack
-OregonH.UI.showAttack = function(firepower, gold) {
-  var attackDiv = document.getElementById('attack');
-  attackDiv.classList.remove('hidden');
- 
-  //keep properties
-  this.firepower = firepower;
-  this.gold = gold;
- 
-  //show firepower
-  document.getElementById('attack-description').innerHTML = 'Firepower: ' + firepower;
- 
-  //init once
-  if(!this.attackInitiated) {
- 
-    //fight
+
+class UI {
+  constructor(game) {
+    this.game = game;
+
+    this.firepower = 0;
+    this.gold = 0;
+
+    // add event listeners for fight dialog box
+    // fight
+
+    console.log('Init UI');
     document.getElementById('fight').addEventListener('click', this.fight.bind(this));
- 
-    //run away
+    // run away
     document.getElementById('runaway').addEventListener('click', this.runaway.bind(this));
- 
-    this.attackInitiated = true;
   }
-};
- 
-//fight
-OregonH.UI.fight = function(){
- 
-  var firepower = this.firepower;
-  var gold = this.gold;
- 
-  var damage = Math.ceil(Math.max(0, firepower * 2 * Math.random() - this.caravan.firepower));
- 
-  //check there are survivors
-  if(damage < this.caravan.crew) {
-    this.caravan.crew -= damage;
-    this.caravan.money += gold;
-    this.notify(damage + ' people were killed fighting', 'negative');
-    this.notify('Found $' + gold, 'gold');
-  }
-  else {
-    this.caravan.crew = 0;
-    this.notify('Everybody died in the fight', 'negative');
-  }
- 
-  //resume journey
-  document.getElementById('attack').classList.add('hidden');
-  this.game.resumeJourney();
-};
- 
-//runing away from enemy
-OregonH.UI.runaway = function(){
- 
-  var firepower = this.firepower;
- 
-  var damage = Math.ceil(Math.max(0, firepower * Math.random()/2));
- 
-  //check there are survivors
-  if(damage < this.caravan.crew) {
-    this.caravan.crew -= damage;
-    this.notify(damage + ' people were killed running', 'negative');
-  }
-  else {
-    this.caravan.crew = 0;
-    this.notify('Everybody died running away', 'negative');
-  }
- 
-  //remove event listener
-  document.getElementById('runaway').removeEventListener('click');
- 
-  //resume journey
-  document.getElementById('attack').classList.add('hidden');
-  this.game.resumeJourney();
- 
-};
 
-//show shop
-OregonH.UI.showShop = function(products){
- 
-  //get shop area
-  var shopDiv = document.getElementById('shop');
-  shopDiv.classList.remove('hidden');
- 
-  //init the shop just once
-  if(!this.shopInitiated) {
- 
-    //event delegation
-    shopDiv.addEventListener('click', function(e){
-      //what was clicked
-      var target = e.target || e.src;
- 
-      //exit button
-      if(target.tagName == 'BUTTON') {
-        //resume journey
-        shopDiv.classList.add('hidden');
-        OregonH.UI.game.resumeJourney();
-      }
-      else if(target.tagName == 'DIV' && target.className.match(/product/)) {
- 
-        OregonH.UI.buyProduct({
-          item: target.getAttribute('data-item'),
-          qty: target.getAttribute('data-qty'),
-          price: target.getAttribute('data-price')
-        });
- 
-      }
-    });
- 
-    this.shopInitiated = true;
+  notify(message, type) {
+    document.getElementById('updates-area').innerHTML = `<div class="update-${type}">Day ${Math.ceil(this.game.caravan.day)}: ${message}</div>${document.getElementById('updates-area').innerHTML}`;
   }
- 
-  //clear existing content
-  var prodsDiv = document.getElementById('prods');
-  prodsDiv.innerHTML = '';
- 
-  //show products
-  var product;
-  for(var i=0; i < products.length; i++) {
-    product = products[i];
-    prodsDiv.innerHTML += '<div class="product" data-qty="' + product.qty + '" data-item="' + product.item + '" data-price="' + product.price + '">' + product.qty + ' ' + product.item + ' - $' + product.price + '</div>';
+
+  refreshStats() {
+    // modify the dom
+    document.getElementById('stat-day').innerHTML = Math.ceil(this.game.caravan.day);
+    document.getElementById('stat-distance').innerHTML = Math.floor(this.game.caravan.distance);
+    document.getElementById('stat-crew').innerHTML = this.game.caravan.crew;
+    document.getElementById('stat-oxen').innerHTML = this.game.caravan.oxen;
+    document.getElementById('stat-food').innerHTML = Math.ceil(this.caravan.food);
+    document.getElementById('stat-money').innerHTML = this.game.caravan.money;
+    document.getElementById('stat-firepower').innerHTML = this.game.caravan.firepower;
+    document.getElementById('stat-weight').innerHTML = `${Math.ceil(this.game.caravan.weight)}/${this.game.caravan.capacity}`;
+
+    // update caravan position
+    document.getElementById('caravan').style.left = `${380 * this.game.caravan.distance / this.game.FINAL_DISTANCE}px`;
   }
-};
- 
-//buy product
-OregonH.UI.buyProduct = function(product) {
-  //check we can afford it
-  if(product.price > OregonH.UI.caravan.money) {
-    OregonH.UI.notify('Not enough money', 'negative');
-    return false;
+
+  showAttack(firepower, gold) {
+    const attackDiv = document.getElementById('attack');
+    attackDiv.classList.remove('hidden');
+
+    this.firepower = firepower;
+    this.gold = gold;
+
+    // keep properties
+    this.game.caravan.firepower = firepower;
+    this.game.caravan.gold = gold;
+
+    console.log('show attack');
+    console.log(firepower, gold);
+
+    // show firepower
+    document.getElementById('attack-description').innerHTML = `Firepower: ${firepower}`;
   }
- 
-  OregonH.UI.caravan.money -= product.price;
- 
-  OregonH.UI.caravan[product.item] += +product.qty;
- 
-  OregonH.UI.notify('Bought ' + product.qty + ' x ' + product.item, 'positive');
- 
-  //update weight
-  OregonH.UI.caravan.updateWeight();
- 
-  //update visuals
-  OregonH.UI.refreshStats();
-};
+
+
+  fight() {
+    const { firepower, money, crew } = this.game.caravan;
+    const { gold, firepower: enemyFirepower } = this; // { firepower }
+
+    const damage = Math.ceil(Math.max(0, firepower * 2 * Math.random() - enemyFirepower));
+
+    console.log('fight!');
+    console.log(enemyFirepower, money, crew, damage);
+
+    // check there are survivors
+    if (damage < crew) {
+      this.game.caravan.crew -= damage;
+      this.game.caravan.money += gold;
+      this.notify(`${damage} people were killed fighting`, 'negative');
+      this.notify(`Found $${gold}`, 'gold');
+    } else {
+      this.game.caravan.crew = 0;
+      this.notify('Everybody died in the fight', 'negative');
+    }
+
+    // resume journey
+    document.getElementById('attack').classList.add('hidden');
+    this.game.resumeJourney();
+  }
+
+  runaway() {
+    const { firepower } = this;
+
+    const damage = Math.ceil(Math.max(0, firepower * Math.random() / 2));
+
+    // check there are survivors
+    if (damage < this.game.caravan.crew) {
+      this.game.caravan.crew -= damage;
+      this.notify(`${damage} people were killed running`, 'negative');
+    } else {
+      this.game.caravan.crew = 0;
+      this.notify('Everybody died running away', 'negative');
+    }
+
+
+    // resume journey
+    document.getElementById('attack').classList.add('hidden');
+    this.game.resumeJourney();
+  }
+
+  showShop(products) {
+    // get shop area
+    const shopDiv = document.getElementById('shop');
+    shopDiv.classList.remove('hidden');
+
+    // init the shop just once
+    if (!this.shopInitiated) {
+    // event delegation
+      shopDiv.addEventListener('click', (e) => {
+      // what was clicked
+        const target = e.target || e.src;
+
+        // exit button
+        if (target.tagName === 'BUTTON') {
+        // resume journey
+          shopDiv.classList.add('hidden');
+          this.game.resumeJourney();
+        } else if (target.tagName === 'DIV' && target.className.match(/product/)) {
+          this.buyProduct({
+            item: target.getAttribute('data-item'),
+            qty: target.getAttribute('data-qty'),
+            price: target.getAttribute('data-price'),
+          });
+        }
+      });
+
+      this.shopInitiated = true;
+    }
+
+    // clear existing content
+    const prodsDiv = document.getElementById('prods');
+    prodsDiv.innerHTML = '';
+
+    // show products
+    let product;
+    for (let i = 0; i < products.length; i += 1) {
+      product = products[i];
+      prodsDiv.innerHTML += `<div class="product" data-qty="${product.qty}" data-item="${product.item}" data-price="${product.price}">${product.qty} ${product.item} - $${product.price}</div>`;
+    }
+  }
+
+  // eslint-disable-next-line consistent-return
+  buyProduct(product) {
+  // check we can afford it
+    if (product.price > this.game.caravan.money) {
+      this.notify('Not enough money', 'negative');
+      return false;
+    }
+
+    this.game.caravan.money -= product.price;
+
+    this.game.caravan[product.item] += +product.qty;
+
+    this.notify(`Bought ${product.qty} x ${product.item}`, 'positive');
+
+    // update weight
+    this.game.caravan.updateWeight();
+
+    // update visuals
+    this.refreshStats();
+  }
+}
+
+// show a notification in the message area
+// OregonH.UI.notify = function (message, type) {
+
+// };
+
+// // refresh visual caravan stats
+// OregonH.UI.refreshStats = function () {
+
+// };
+
+// show attack
+// OregonH.UI.showAttack = function (firepower, gold) {
+
+// };
+
+// fight
+// OregonH.UI.fight = function () {
+
+// };
+
+// runing away from enemy
+// OregonH.UI.runaway = function () {
+
+// };
+
+// show shop
+// OregonH.UI.showShop = function (products) {
+
+// };
+
+// buy product
+// OregonH.UI.buyProduct = function (product) {
+
+// };
